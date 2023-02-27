685708e0e0ac19faadb088f4cd934557dfc8c3cf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-27 22:16:46 +0530

    Code Refactoring

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'dashboard', loadComponent:
       () => import('./dashboard/dashboard.component').then((c) => c.DashboardComponent)
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   standalone: true,
   selector: 'app-home',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }
